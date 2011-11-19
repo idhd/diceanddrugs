343b3ba107c052cf2a31f823a2dde928cbb2dfe4
Paul Chobert <paul@chobert.fr>
2011-11-19 10:59:09 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diceanddrugs::Application.config.secret_token = '02a39082ff8da6e8a7f5bb8728c352d6d5318a916fe34cfb60f4286e859f915a4fd9c00a496ad229d9ee923b9eee45300e609b3a0d0a1018f681efb32df597c2'
